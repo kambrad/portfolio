4c7e1b32edb10f6291169797ac59d26ed2548f08
Kam Brad <78763169+kambrad@users.noreply.github.com>
2024-08-15 04:38:21 -0400

    Add files via upload

---
+++ landing/two/lib/js/util/geo.js
@@ -0,0 +1,16 @@
+// let map;
+
+
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: 35.824373, lng: -78.619874 },
+//     zoom: 8,
+//   });
+
+//   log (map);
+  
+// }
+
+// initMap();
